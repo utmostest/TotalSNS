9e0b6ddf4c8dcf026c3cf0488e73aed8be86d7dc
utmostest <utmostest@gmail.com>
2018-12-20 19:44:43 +0900

    twitter-kit-android -> twitter4j로 변경 트위터킷의 특정부분 제한때문에 교체함 (트윗 작성시 트위터킷의 액티비티 사용 등) 불필요한 파일과 코드 제거

--- app/src/main/java/com/enos/totalsns/util/TwitterUtil.java
+++ app/src/main/java/com/enos/totalsns/util/TwitterUtil.java
@@ -2,19 +2,6 @@ package com.enos.totalsns.util;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.util.Log;
-
-import com.enos.totalsns.BuildConfig;
-import com.twitter.sdk.android.core.DefaultLogger;
-import com.twitter.sdk.android.core.Twitter;
-import com.twitter.sdk.android.core.TwitterApiClient;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterConfig;
-import com.twitter.sdk.android.core.TwitterCore;
-import com.twitter.sdk.android.core.TwitterSession;
-
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 
 public class TwitterUtil {
 
@@ -32,28 +19,6 @@ public class TwitterUtil {
     }
 
     public void init(@NonNull Context context, @NonNull String consumerKey, @NonNull String consumerSecret) {
-        TwitterConfig config = new TwitterConfig.Builder(context.getApplicationContext())
-                .logger(new DefaultLogger(Log.DEBUG))
-                .twitterAuthConfig(new TwitterAuthConfig(consumerKey, consumerSecret))
-                .debug(BuildConfig.DEBUG)
-                .build();
-        Twitter.initialize(config);
 
-        final HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
-        final OkHttpClient customClient = new OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor).build();
-
-        final TwitterSession activeSession = TwitterCore.getInstance()
-                .getSessionManager().getActiveSession();
-
-        final TwitterApiClient customApiClient;
-        if (activeSession != null) {
-            customApiClient = new TwitterApiClient(activeSession, customClient);
-            TwitterCore.getInstance().addApiClient(activeSession, customApiClient);
-        } else {
-            customApiClient = new TwitterApiClient(customClient);
-            TwitterCore.getInstance().addGuestApiClient(customApiClient);
-        }
     }
 }
