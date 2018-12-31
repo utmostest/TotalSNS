9cbc387772761e0fb7f642915aa23b6a7f92c6f3
utmostest <utmostest@gmail.com>
2018-12-31 18:07:12 +0900

    이메일 정규식 표현 변경

--- app/src/main/java/com/enos/totalsns/util/autolink/RegexParser.java
+++ app/src/main/java/com/enos/totalsns/util/autolink/RegexParser.java
@@ -1,7 +1,5 @@
 package com.enos.totalsns.util.autolink;
 
-import android.util.Patterns;
-
 /**
  * Created by Chatikyan on 25.09.2016-19:23.
  */
@@ -11,12 +9,12 @@ class RegexParser {
     static final String PHONE_PATTERN = "\\d{2,3}-\\d{3,4}-\\d{4}|\\d{9,16}|\\+\\d{2,3}-\\d{2,3}" +
             "-\\d{3,4}-\\d{4}|\\d{2,3}-\\d{2,3}-\\d{3,4}-\\d{4}|\\d{4}-\\d{4}";
     //    static final String PHONE_PATTERN = Patterns.PHONE.pattern();
-//    static final String EMAIL_PATTERN = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|" +
-//            "\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")" +
-//            "@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.)" +
-//            "{3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]" +
-//            "|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-    static final String EMAIL_PATTERN = Patterns.EMAIL_ADDRESS.pattern();
+    static final String EMAIL_PATTERN = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|" +
+            "\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")" +
+            "@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.)" +
+            "{3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]" +
+            "|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+    //    static final String EMAIL_PATTERN = Patterns.EMAIL_ADDRESS.pattern();
     static final String HASHTAG_PATTERN = "(?:^|\\s|$)#[\\p{L}0-9_]*";
     static final String MENTION_PATTERN = "(?:^|\\s|$|[.])@[\\p{L}0-9_]*";
     static final String URL_PATTERN = "(^|[\\s.:;?\\-\\]<\\(])" +
