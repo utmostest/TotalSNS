b37f8126a93232fe635d8cb28fa57f62a6fbc2ea
enos <utmostest@gmail.com>
2022-11-25 18:38:23 +0900

    오류로 인해 멀티덱스 추가

--- app/src/main/java/com/enos/totalsns/TotalSNSApp.java
+++ app/src/main/java/com/enos/totalsns/TotalSNSApp.java
@@ -2,9 +2,11 @@ package com.enos.totalsns;
 
 import android.app.Application;
 
+import androidx.multidex.MultiDexApplication;
+
 import com.enos.totalsns.util.SingletonToast;
 
-public class TotalSNSApp extends Application {
+public class TotalSNSApp extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
